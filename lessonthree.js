8af9ce5edc9be3843c7bf015836d31753fe85efb
pavii0 <pavithra005@e.ntu.edu.sg>
2025-05-17 21:12:17 +0800

    lesson3

---
+++ lessonthree.js
@@ -0,0 +1,22 @@
+const stockOHLC = {
+	open: 81.12,
+	high: 82.59,
+	low: 80.09,
+	close: 84.71,
+}
+
+// Write your code below
+
+if (stockOHLC.open === stockOHLC.close){
+	console.log("they are equal")
+}
+else if (stockOHLC.open > stockOHLC.close){
+	console.log("Stock is bearish")
+}
+else{
+	console.log("Stock is bullish")
+}
+
+
+
+
